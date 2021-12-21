87dae21bee9a2d99d93662503c131bf9ddae9294
vivek20ingh <vs2357766@gmail.com>
2021-12-21 23:12:06 +0530

    update

--- src/Form.js
+++ src/Form.js
@@ -1,124 +1,100 @@
-import React,  {useState,useEffect}  from "react";
-import { ListGroup } from "react-bootstrap";
+import React,  {useState}  from "react";
+
 import './Styles.css'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"></link>
-
-
+import Listview from "./Listview";
+import { ListGroup } from "react-bootstrap";
+import shortid from "shortid";
 
 
  const Form=() => {
-    const [name, setName] = useState("");
-    const [age, setAge] = useState("");
-    const [city, setCity] = useState("");
+    const [object,setobject]=useState({'unique_id':null,'name':'','city':'','age':''});
     const [masterData,setmasterData]= useState([]);
-    const [changeButtonm, setChangeButton]=useState(0);
     const [buttonName, setButtonName]=useState("Submit")
-    const [key, setkey]=useState(0);
-   
+
     const handleSubmit = (evt) => {
         evt.preventDefault();
-        
-        var obj={'name':name,'city': city, 'age': age};
-        
-        setmasterData([...masterData,
-            obj
-        ]);
+        let key= shortid.generate();
+        object.unique_id=key;
+        setobject(object)
+        setmasterData([...masterData,object]);
+        console.log(object)
+        console.log(key);
+        setobject({...object,unique_id:null,name:'',city:'',age:''})
     }
 
     const handleDelete =(idx)=>{
-        masterData.splice(idx, 1);
+        console.log(idx);
+        let newidx=masterData.findIndex(obj => obj.unique_id===idx );
+        masterData.splice(newidx, 1);
             setmasterData([...masterData])
-
-            
     }
 
     const handleUpdate =(idx)=>{
-        setChangeButton(1)
-        setName(masterData[idx].name)
-        setCity(masterData[idx].city)
-        setAge(masterData[idx].age)
-        setButtonName("Update")
-        setkey(idx);
-
-       
-
+        let newidx=masterData.findIndex(obj => obj.unique_id===idx );
+        setobject({unique_id:idx,name:masterData[newidx].name, city: masterData[newidx].city,age:masterData[newidx].age})
+        setButtonName("Update")   
     }
+
 const handleEdit=(e)=>{
     e.preventDefault()
-   let obj1={
-       name:name,
-       city:city,
-       age:age
-   }
-   masterData[key]=obj1;
-   setmasterData([...masterData])
-   setChangeButton(0)
+   let obj1=object
+   let newidx=masterData.findIndex(obj => obj.unique_id===obj1.unique_id);
+   masterData[newidx]=obj1;
+   setmasterData(masterData)
+   setobject({...object,unique_id:null,name:'',city:'',age:''})
    setButtonName("Submit")
 }
         return (
             <div>
                  <form class="split left">
                      <h3>Detail</h3>
-
                      <div className="form-group">
                           <label>Name</label>
                           <input type="text" className="form-control" placeholder="Enter Name" 
-                          value= {name}
-                          onChange={e=> setName(e.target.value)}/>
+                          value= {object.name}
+                          onChange={e=> {
+                              setobject({...object,name: e.target.value}); }} />
                      </div>
 
                      <div className="form-group" style={{marginTop: 10}}>
                           <label >City Name</label>
                           <input type="text" className="form-control" placeholder="Enter City Name"  
-                          value= {city}
-                          onChange={e=> setCity(e.target.value)}/>
+                          value= {object.city}
+                          onChange={e=> {
+                             setobject({...object,city: e.target.value});  }}/>
                      </div>
-
-               
-
 
                      <div className="form-group" style={{marginTop: 10}}>
                          <label >Age</label>
                          <input type="text" className="form-control" placeholder="Enter Your Age" 
-                          value= {age}
-                          onChange={e=> setAge(e.target.value)}/> 
+                          value= {object.age}
+                          onChange={e=> {
+                            setobject({...object,age: e.target.value}); }}/>    
                     </div>
-                    <button  onClick={changeButtonm==0?(evt)=>handleSubmit(evt):(e)=>handleEdit(e) } className="btn btn-primary btn-block"  style={{marginTop: 10}}>{buttonName}</button>
-                   
+                    <button onClick={object.unique_id==null?(evt)=>handleSubmit(evt):(e)=>handleEdit(e) } className="btn btn-primary btn-block"  style={{marginTop: 10}}>{buttonName}</button>
                  </form>
 
-                <div class="split right"> 
-              
-                    {masterData.map((d,idx)=>
-                          <ListGroup as="ol" >
-                        <ListGroup.Item
-                          as="li" className="d-flex justify-content-between align-items-start"react js >
-                              <div className="ms-2 me-auto">
-                                 <div className="fw-bold">NAME: {d.name}</div>
-                                <div className="fw-bold">CITY: {d.city}</div>
-                                <div className="fw-bold">AGE: {d.age}</div>
-                                <button onClick ={()=>handleDelete(idx)}>Delete</button>
-                                 <button onClick={()=>
-                                    handleUpdate(idx)
-                                    } >Update</button>
-
-                            </div>
-                            
-                  
-                </ListGroup.Item>
-                </ListGroup>
-                )}
-                
-                </div>
-                
+                 <div class="split right"> 
+              .
+              {masterData.map((d,idx)=>
+              <ListGroup as="ol" key={d.unique_id} >
+                  <ListGroup.Item
+                    as="li" className="d-flex justify-content-between align-items-start"react js >
+                        <div className="ms-2 me-auto">
+                             <div className="fw-bold">NAME: {d.name}</div>
+                             <div className="fw-bold">CITY: {d.city}</div>
+                              <div className="fw-bold">AGE: {d.age}</div>
+                              <button onClick ={()=>handleDelete(d.unique_id)}>Delete</button>
+                              <button onClick={()=>handleUpdate(d.unique_id)} >Update</button>
+                      </div>
+                   </ListGroup.Item>
+                 </ListGroup> )}
+          </div>
+                 {/* <Listview x={setmasterData,setButtonName,setobject}/> */}
             </div>
-
-
         );
 }
 
---
+++ src/Listview.js
@@ -0,0 +1,41 @@
+import { ListGroup } from "react-bootstrap";
+
+const Listview =({setmasterData,setButtonName,setobject})=>{
+
+    let masterData=setmasterData
+    const handleDelete =(idx)=>{
+        console.log(idx);
+        let newidx=masterData.findIndex(obj => obj.unique_id===idx );
+        masterData.splice(newidx, 1);
+            setmasterData([...masterData])
+    }
+
+    const handleUpdate =(idx)=>{
+        let newidx=masterData.findIndex(obj => obj.unique_id===idx );
+        setobject({unique_id:idx,name:masterData[newidx].name, city: masterData[newidx].city,age:masterData[newidx].age})
+        setButtonName("Update")   
+    }
+
+
+    return(
+
+        <div class="split right"> 
+        .
+        {masterData.map((d,idx)=>
+        <ListGroup as="ol" key={d.unique_id} >
+            <ListGroup.Item
+              as="li" className="d-flex justify-content-between align-items-start"react js >
+                  <div className="ms-2 me-auto">
+                       <div className="fw-bold">NAME: {d.name}</div>
+                       <div className="fw-bold">CITY: {d.city}</div>
+                        <div className="fw-bold">AGE: {d.age}</div>
+                        <button onClick ={()=>handleDelete(d.unique_id)}>Delete</button>
+                        <button onClick={()=>handleUpdate(d.unique_id)} >Update</button>
+                </div>
+             </ListGroup.Item>
+           </ListGroup> )}
+       </div>
+    )
+}
+
+export default  Listview;
